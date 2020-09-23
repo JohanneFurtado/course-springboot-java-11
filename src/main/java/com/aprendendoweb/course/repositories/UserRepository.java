fde62e69eb498eda2fc944ce954143ecb2fe5fbe
JohanneFurtado <apollonmonteiro@gmail.com>
2020-09-23 21:01:22 +0100

    Service layer, component registration

--- src/main/java/com/aprendendoweb/course/repositories/UserRepository.java
+++ src/main/java/com/aprendendoweb/course/repositories/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aprendendoweb.course.entities.User;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/aprendendoweb/course/services/UserService.java
@@ -0,0 +1,30 @@
+package com.aprendendoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aprendendoweb.course.entities.User;
+import com.aprendendoweb.course.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+
+}
