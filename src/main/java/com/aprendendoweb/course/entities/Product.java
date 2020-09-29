87c74f296c737617118948fb2fc739d1e2ab1d22
JohanneFurtado <apollonmonteiro@gmail.com>
2020-09-29 02:22:46 +0100

    Payment, one-to-one association

--- src/main/java/com/aprendendoweb/course/entities/Product.java
+++ src/main/java/com/aprendendoweb/course/entities/Product.java
@@ -14,6 +14,8 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
@@ -92,6 +94,7 @@ public class Product implements Serializable {
 		return categories;
 	}
 	
+	@JsonIgnore
 	public Set<Order> getOrders(){
 		Set<Order>set = new HashSet<>();
 		for(OrderItem x : items) {
