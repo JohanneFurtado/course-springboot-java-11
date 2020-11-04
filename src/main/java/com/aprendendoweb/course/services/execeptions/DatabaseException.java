acfac6820aef304237632b98e9f0199ef3af86a2
JohanneFurtado <apollonmonteiro@gmail.com>
2020-11-04 12:08:25 +0000

    Exception handling - delete

---
+++ src/main/java/com/aprendendoweb/course/services/execeptions/DatabaseException.java
@@ -0,0 +1,16 @@
+package com.aprendendoweb.course.services.execeptions;
+
+public class DatabaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+
+
+	
+	
+	
+
+}
